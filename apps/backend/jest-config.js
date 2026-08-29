2b436202d61dc6ca040d49c78a078af1fd45d73b
YazirDev <zunigaalejandro878@gmail.com>
2026-08-29 11:52:58 -0600

    feat: add initial configuration files for Jest, TypeScript, and SonarCloud

---
+++ apps/backend/jest-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.spec.ts',
+    '!**/index.ts',
+    '!**/*.dto.ts',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+}
